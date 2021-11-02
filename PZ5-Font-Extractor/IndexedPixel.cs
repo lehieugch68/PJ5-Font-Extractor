a6970fbc702f10a90ac9d5894917bb57f96b3e25
Lê Hiếu <51288927+lehieugch68@users.noreply.github.com>
2021-11-02 22:15:08 +0700

    Update

--- PZ5-Font-Extractor/IndexedPixel.cs
+++ PZ5-Font-Extractor/IndexedPixel.cs
@@ -18,22 +18,17 @@ namespace PZ5_Font_Extractor
             rd.BaseStream.Seek(128, SeekOrigin.Begin);
             while (rd.BaseStream.Length > rd.BaseStream.Position)
             {
-                byte high = rd.ReadByte();
-                byte low = rd.ReadByte();
+                string color = string.Empty;
+                color += JoinBit(rd.ReadByte());
+                color += JoinBit(rd.ReadByte());
 
-                var b = (high << 4) | low;
-
-                wt.Write((byte)b);
-                /*string value = string.Empty;
-                byte bit_a = rd.ReadByte();
-                byte bit_b = rd.ReadByte();
-                value += $"{Compare(bit_b)}{Compare(bit_a)}";
-                wt.Write(Convert.ToByte(value, 2));*/
+                wt.Write(byte.Parse(color, System.Globalization.NumberStyles.HexNumber));
             }
             wt.Close();
             rd.Close();
             return result.ToArray();
         }
+
         private static string Compare(byte index)
         {
             if (index > 225) index = 225;
@@ -41,6 +36,7 @@ namespace PZ5_Font_Extractor
             string value = Convert.ToString(square, 2).PadLeft(4, '0');
             return value;
         }
+
         public static byte[] Convert4BppTo8Bpp(byte[] input)
         {
             MemoryStream stream = new MemoryStream(input);
@@ -49,20 +45,51 @@ namespace PZ5_Font_Extractor
             BinaryWriter wt = new BinaryWriter(result);
             while (rd.BaseStream.Length > rd.BaseStream.Position)
             {
-                byte bit = rd.ReadByte();
-                /*int low = bit & 0xF;
-                int high = bit >> 4;
-                wt.Write((byte)high);
-                wt.Write((byte)low);*/
-                char[] chars = Convert.ToString(bit, 2).PadLeft(8, '0').ToCharArray();
-                int firstHalfByte = (int)Math.Pow(int.Parse(new string(chars.Take(4).ToArray()).PadLeft(8, '0')), 2);
-                int lastHalfByte = (int)Math.Pow(int.Parse(new string(chars.Skip(4).Take(4).ToArray()).PadLeft(8, '0')), 2);
-                wt.Write(Convert.ToByte(firstHalfByte > 255 ? 255 : firstHalfByte));
-                wt.Write(Convert.ToByte(lastHalfByte > 255 ? 255 : lastHalfByte));
+                string bit = rd.ReadByte().ToString("X2");
+                wt.Write(byte.Parse($"{bit[0]}0", System.Globalization.NumberStyles.HexNumber));
+                wt.Write(byte.Parse($"{bit[1]}0", System.Globalization.NumberStyles.HexNumber));
             }
             wt.Close();
             rd.Close();
             return result.ToArray();
+        }
+
+        private static string JoinBit(byte input)
+        {
+            string res = string.Empty;
+            if (input <= 0x0F)
+                res = "0";
+            if (0x0F < input && input <= 0x20)
+                res = "1";
+            if (0x20 < input && input <= 0x30)
+                res = "2";
+            if (0x30 < input && input <= 0x40)
+                res = "3";
+            if (0x40 < input && input <= 0x50)
+                res = "4";
+            if (0x50 < input && input <= 0x60)
+                res = "5";
+            if (0x60 < input && input <= 0x70)
+                res = "6";
+            if (0x70 < input && input <= 0x80)
+                res = "7";
+            if (0x80 < input && input <= 0x90)
+                res = "8";
+            if (0x90 < input && input <= 0xA0)
+                res = "9";
+            if (0xA0 < input && input <= 0xB0)
+                res = "A";
+            if (0xB0 < input && input <= 0xC0)
+                res = "B";
+            if (0xC0 < input && input <= 0xD0)
+                res = "C";
+            if (0xD0 < input && input <= 0xE0)
+                res = "D";
+            if (0xE0 < input && input <= 0xF0)
+                res = "E";
+            if (0xF0 < input && input <= 0xFF)
+                res = "F";
+            return res;
         }
     }
 }
